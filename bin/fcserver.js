e847912469ef46fac7295d0de0b4923884d199cf
wenber <wenbo.fe@gmail.com>
2015-09-18 17:32:16 +0800

    to 0.0.1

--- bin/fcserver.js
+++ bin/fcserver.js
@@ -10,10 +10,19 @@
 var program = require('commander');
 
 program
-    .version('0.0.0');
+    .version('0.0.0')
+    .option('-p, --port', 'set the listen port');
 
 program
     .command('start')
-    .action(function () {
-        require('../lib/server').start();
+    .action(function (port) {
+        var server = require('../lib/server')
+        if (program.port) {
+            server.start(port);
+        }
+        else {
+            server.start();
+        }
     });
+
+program.parse(process.argv);
