7bb3796cfae4b409c6defc186c20306ef43a342b
wenber <wenbo.fe@gmail.com>
2015-09-24 10:45:24 +0800

    支持moc数据服务

--- Readme.md
+++ Readme.md
@@ -9,9 +9,10 @@ npm install -g fcserver
 ### 使用
 + 启动方法
 ```
-fcserver start [-o]   // -o 表示同时打开默认浏览器
+fcserver start [-o] [-p 8000]   // -o 表示同时打开默认浏览器 -p 指定端口
 ```
 
+### 功能
 + 支持文件目录索引
 + 支持less文件使用
 + 正在扩展各种资源的处理器...
---
+++ fcserver-config.js
@@ -0,0 +1,17 @@
+exports.path2Handler = [
+    { 
+        path: /.+\.less($|\?)/,
+        handler: [
+            'less'
+        ]
+    },
+    {
+        path: /.+\.ajax($|\?)/,
+        handler: [
+            'mock'
+        ]
+    }
+];
+
+// 默认是服务启动目录下的mock文件夹，这里也支持自定义的mock目录
+exports.mockPath = './mock';
