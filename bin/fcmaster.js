fd15d70ea8019b712e9980d548a220aa0d56a584
wenber <wenbo.fe@gmail.com>
2016-01-06 10:29:03 +0800

    新增跌机重启控制

--- bin/fcmaster.js
+++ bin/fcmaster.js
@@ -9,11 +9,23 @@ var server = require('net').createServer();
 server.listen(1337);
 
 var worker = {};
+var exitCount = 0;
+var start = new Date().getTime();
+
 var createWorker = function () {
     var server  = path.resolve(__dirname, './fcserver.js');
     worker = exec('node ' + server + ' start');
+    
 
     worker.on('exit', function (err) {
+        if (++exitCount >= 4) {
+            // 如果1秒内重启了4次，认为系统有严重错误，不在自动重启
+            if (new Date().getTime() - start > 1000){
+                console.log('there is a bad  error!');
+                process.exit(-1);
+            }
+        }
+        exitCount++;
         console.log('Worker ' + worker.pid + ' exited' + err);
         delete worker;
         createWorker();
